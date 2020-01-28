4a93d8cbf82ce527d9a7615fc602947a4ecee58e
Humphrey Codrington <humphrey.codrington@gmail.com>
2020-01-28 13:14:30 +0000

    comments routes and controller added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :issues, only: [ :index, :show ]
+  resources :issues, only: [ :index, :show ] do
+    resources :comments, only: :create
+  end
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
