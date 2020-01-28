7b53d83d7987049ee4ffa5f67868fb928be8dae2
Humphrey Codrington <humphrey.codrington@gmail.com>
2020-01-28 13:20:24 +0000

    controller methods added

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,22 @@
 class CommentsController < ApplicationController
+  def create
+    @issue = Issue.find(params[:issue_id])
+    @comment = comment.new(comment_params)
+    @comment.issue = @issue
+    if @issue.save
+      redirect_to issue_path(@issue)
+    else
+      render 'issue/show'
+    end
+
+  def show
+    @issue = Issue.find(params[:id])
+    @comment = Comment.new
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 end
